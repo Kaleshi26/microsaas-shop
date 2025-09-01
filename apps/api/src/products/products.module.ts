2b6d6000a4aafb2640fd1bec50e81d410f247e09
Kalana Dharmarathne <kalanaeshan07@gmail.com>
2025-09-01 20:52:38 +0530

    Add ProductsModule with GraphQL and Apollo integration
    Introduces the ProductsModule, integrating GraphQL using ApolloDriver, and registers ProductsService, ProductsController, PrismaService, and ProductsResolver. This sets up the foundation for product-related API and GraphQL functionality.

--- apps/api/src/products/products.module.ts
+++ apps/api/src/products/products.module.ts
@@ -1,1 +1,20 @@
-﻿
+﻿import { Module } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { ProductsController } from './products.controller';
+import { PrismaService } from '../prisma/prisma.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ProductsResolver } from './products.resolver';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      path: '/graphql'
+    })
+  ],
+  providers: [ProductsService, PrismaService, ProductsResolver],
+  controllers: [ProductsController]
+})
+export class ProductsModule {}
