bde7435795e0c2e22579ac6c4d6d2c76439944e6
Kalana Dharmarathne <kalanaeshan07@gmail.com>
2025-09-01 21:11:58 +0530

    Add Terraform config for AWS Lambda and API Gateway
    Introduces Terraform resources to provision a Node.js Lambda function, IAM role, API Gateway HTTP API, integration, route, and permissions for local development using LocalStack endpoints. Also outputs the API invoke URL.

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,1 +1,84 @@
-﻿
+﻿terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.4"
+    }
+  }
+}
+
+provider "aws" {
+  region                      = var.region
+  access_key                  = "test"
+  secret_key                  = "test"
+  s3_force_path_style         = true
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  endpoints {
+    apigateway = "http://localhost:4566"
+    lambda     = "http://localhost:4566"
+    iam        = "http://localhost:4566"
+    s3         = "http://localhost:4566"
+  }
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_exec_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
+  })
+}
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/lambda"
+  output_path = "${path.module}/lambda.zip"
+}
+
+resource "aws_lambda_function" "invoice" {
+  function_name = "invoice"
+  role          = aws_iam_role.lambda_exec.arn
+  runtime       = "nodejs20.x"
+  handler       = "invoice.handler"
+  filename      = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+}
+
+resource "aws_apigatewayv2_api" "http" {
+  name          = "invoice-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda" {
+  api_id                 = aws_apigatewayv2_api.http.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.invoice.invoke_arn
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "post_invoice" {
+  api_id    = aws_apigatewayv2_api.http.id
+  route_key = "POST /invoice"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+}
+
+resource "aws_lambda_permission" "allow_apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.invoice.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.http.execution_arn}/*/*"
+}
+
+output "invoke_url" {
+  value = aws_apigatewayv2_api.http.api_endpoint
+}
