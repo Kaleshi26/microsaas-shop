8cd22030c61dbbc005c58b353ac16a6b00eb6a46
Kalana Dharmarathne <kalanaeshan07@gmail.com>
2025-09-01 20:53:52 +0530

    Add ProductsResolver with products query
    Introduces the ProductsResolver class to expose a GraphQL query for listing products using the ProductsService.

--- apps/api/src/products/products.resolver.ts
+++ apps/api/src/products/products.resolver.ts
@@ -1,1 +1,12 @@
-﻿
+﻿import { Query, Resolver } from '@nestjs/graphql';
+import { Product } from './product.model';
+import { ProductsService } from './products.service';
+
+@Resolver(() => Product)
+export class ProductsResolver {
+  constructor(private svc: ProductsService) {}
+  @Query(() => [Product])
+  async products(): Promise<Product[]> {
+    return this.svc.list();
+  }
+}
